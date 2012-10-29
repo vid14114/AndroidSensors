1a235c9db01dacf089552b655b90513ea468e536
Viktor Vidovic <vid14114@spengergasse.at>
2012-10-29 20:22:10 +0100

    Sensors registered

--- src/output/PhoneSensor.java
+++ src/output/PhoneSensor.java
@@ -14,13 +14,12 @@ import android.hardware.SensorManager;
 import android.service.textservice.SpellCheckerService.Session;
 
 /**
- * @author abideen
- * Many types of output, mainly twp types: real-time like view, and non real time
+ * Get the values of the user-selected sensors
  */
 public class PhoneSensor extends Activity implements SensorEventListener{		
 	private SensorManager sensorManager;
 	private Sensor sensor;
-	public static float[] movement;
+	public static float[] movementDirection;
 	public static float[] accelerometer;
 	public static float[] magnet;
 	public static float[] direction;
@@ -31,31 +30,48 @@ public class PhoneSensor extends Activity implements SensorEventListener{
 	public PhoneSensor(ArrayList<String> options){
 		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-		//if 
-			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);			
-			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-			sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+		for(String s : options)
+		{
+			if(s.equals("Movement") || s.equals("Direction"))
+			{
+				sensor=sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(s.equals("Acceleration"))
+			{
+				sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(s.equals("Magnet"))
+			{
+				sensor=sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(s.equals("Air Pressure"))
+			{
+				sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(s.equals("Temperature"))
+			{
+				sensor=sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(s.equals("Light"))
+			{
+				sensor=sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+				sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+		}		
 	}
-		
-	/*
-	 * (non-Javadoc)
-	 * @see android.hardware.SensorEventListener#onSensorChanged(android.hardware.SensorEvent)
-	 * public static float[] movement;
-	public static float[] accelerometer;
-	public static float[] magnet;
-	public static float[] direction;
-	public static float[] temperature;
-	public static float[] light;
-	 */
+	
 	public void onSensorChanged(SensorEvent event) { 
 		if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION);
-			movement = event.values;
+			movementDirection = event.values;
 		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER);
 			accelerometer = event.values;
 		if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD);
 			magnet = event.values;
-		if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION);
-			direction = event.values;
 		if(event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE);
 			temperature = event.values;
 		if(event.sensor.getType() == Sensor.TYPE_LIGHT)
